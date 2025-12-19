380ceb3a1fd08e50cda702452c1d1c3368f17aa8
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-12-19 23:47:19 +0800

    Added etl-ingestion scripts

--- etl_ingestion/main.py
+++ etl_ingestion/main.py
@@ -1,5 +1,23 @@
+from weather_api import fetch_data, mock_fetch_data
+from db_connect import connect_db, create_table, insert_records
+
 def main():
     print('Python environment configured!')
+    # print(fetch_data())
+    # print(mock_fetch_data())
+    # print(connect_db())
+    try:
+        data = mock_fetch_data()
+        conn = connect_db()
+        create_table(conn=conn)
+        insert_records(conn=conn, data=data)       
+
+    except Exception as e:
+        print(f'An Error occured during execution: {e}')
+    finally: 
+        if 'conn' in locals():
+            conn.close()
+            print('Database connection closed.')  
 
 
 
--- etl_ingestion/weather_api.py
+++ etl_ingestion/weather_api.py
@@ -1,8 +1,26 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 
-api_url = ''
+load_dotenv()
+
+API_KEY = os.getenv("WEATHERSTACK_API_KEY")
+api_url = f"http://api.weatherstack.com/current?access_key={API_KEY}&query=New York"
 
 def fetch_data():
-    response = requests.get(api_url)
-    print(response)
+    print("Fetching weather data from weatherstack API...")
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        print("API response recieved successfully!")
+        return response.json()
+    
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+        raise    
+
+def mock_fetch_data():
+    return {'request': {'type': 'City', 'query': 'New York, United States of America', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'New York', 'country': 'United States of America', 'region': 'New York', 
+'lat': '40.714', 'lon': '-74.006', 'timezone_id': 'America/New_York', 'localtime': '2025-12-18 08:00', 'localtime_epoch': 1766044800, 'utc_offset': '-5.0'}, 'current': {'observation_time': '01:00 PM', 'temperature': 0, 'weather_code': 113, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png'], 'weather_descriptions': ['Clear '], 'astro': {'sunrise': '07:15 AM', 'sunset': '04:30 PM', 'moonrise': '06:17 AM', 'moonset': '03:05 PM', 'moon_phase': 'Waning Crescent', 'moon_illumination': 4}, 'air_quality': {'co': '792.85', 'no2': '53.25', 'o3': '26', 'so2': '14.45', 'pm2_5': '30.75', 'pm10': '32.15', 'us-epa-index': '2', 'gb-defra-index': '2'}, 'wind_speed': 4, 'wind_degree': 39, 'wind_dir': 'NE', 'pressure': 1026, 'precip': 0, 'humidity': 75, 
+'cloudcover': 25, 'feelslike': -1, 'uv_index': 0, 'visibility': 16, 'is_day': 'yes'}}
