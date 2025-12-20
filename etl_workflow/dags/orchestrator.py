368bd7bd1ba8dd59d5ef72af39ef03f20a0f3681
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-12-20 22:57:57 +0800

    added apache airflow orchestration

---
+++ etl_workflow/dags/orchestrator.py
@@ -0,0 +1,31 @@
+import sys
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+
+
+# sys.path.append('/opt/etl_workflow/etl_ingestion')
+
+def safe_main_callable():
+    from etl_ingestion.main import main
+    return main()
+
+default_args = {
+    'description': 'A DAG to orchestrate data',
+    'start_date': datetime(2025, 12, 1),
+    'catchup':False
+}
+
+dag = DAG(
+    dag_id='weather-api-orchestrator',
+    default_args=default_args,
+    schedule=timedelta(minutes=5)
+)
+
+with dag:
+    task1 = PythonOperator(
+        task_id='ingest_data_task',
+        python_callable=safe_main_callable
+    )
+    #task2
