231e2e6385aed5fd454de9b6f282cf4f0676e1ee
Abubakar Abdullahi <ajanifiles90@gmail.com>
2025-12-23 14:52:49 +0800

    Added apache superset feature

--- etl_workflow/dags/orchestrator.py
+++ etl_workflow/dags/orchestrator.py
@@ -21,7 +21,7 @@ default_args = {
 dag = DAG(
     dag_id='weather-api-dbt-orchestrator',
     default_args=default_args,
-    schedule=timedelta(minutes=1)
+    schedule=timedelta(minutes=5)
 )
 
 with dag:
---
+++ postgres/superset_init.sql
@@ -0,0 +1,10 @@
+
+CREATE USER superset WITH password 'superset';
+
+CREATE DATABASE db_superset OWNER superset;
+
+
+
+CREATE USER examples WITH password 'examples';
+
+CREATE DATABASE db_examples OWNER examples;
